72c30485005820897e5e4eaaa0b24c3c586a87e4
hirose <hirose.motohito@gmail.com>
2022-06-04 19:19:02 +0900

    fix indent

--- boost_asio_http_server.hpp
+++ boost_asio_http_server.hpp
@@ -480,18 +480,18 @@ public:
     explicit server(const std::string& address, const std::string& port, const std::string& docRoot)
         : ioContext_(1), acceptor_(ioContext_), docRoot_(docRoot), valid_(false), acceptorOpened_(false)
     {
-		try {
-			boost::asio::ip::tcp::resolver resolver(ioContext_);
-			boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(address, port).begin();
-			acceptor_.open(endpoint.protocol());
-			acceptor_.bind(endpoint);
-			acceptor_.listen();
-			acceptorOpened_ = true;
+        try {
+            boost::asio::ip::tcp::resolver resolver(ioContext_);
+            boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(address, port).begin();
+            acceptor_.open(endpoint.protocol());
+            acceptor_.bind(endpoint);
+            acceptor_.listen();
+            acceptorOpened_ = true;
 
-			do_accept();
-			valid_ = true;
-		} catch (const boost::system::system_error& ec) {
-		}
+            do_accept();
+            valid_ = true;
+        } catch (const boost::system::system_error& ec) {
+        }
     }
 
     bool is_valid() const { return valid_; }
